4c587232139200dd3397c4d87086da1ead601b53
Paarth Srivastava <118597292+PaarthSuwi@users.noreply.github.com>
2025-04-13 18:28:46 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
 
-model = load_model('model/deepfake_model.h5')
+from utils.preprocessing import load_and_preprocess_image
 
 def predict_image(image_path):
     image = cv2.imread(image_path)
@@ -12,3 +12,9 @@ def predict_image(image_path):
 
     prediction = model.predict(image)[0][0]
     return prediction > 0.5, prediction  # (True = Deepfake, confidence)
+
+image = load_and_preprocess_image("path/to/image.png")
+if image is not None:
+    prediction = model.predict(image)
+
+model = load_model('model/deepfake_model.h5')
